181e4d9d71ed7e7a14298f58c581f5b4a9ced202
Zehra Nur Uğur <221118064@samsun.edu.tr>
2025-09-07 13:00:06 +0300

    Initial commit by Zehra Nur Uğur

---
+++ src/opend_assets/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <div id="footer">
+      <footer>
+        <Container fluid="md">
+          <Row>
+            <Col>
+              <p>
+                "A decentralized platform for digital ownership and creative expression. Built on Internet Computer for seamless NFT experiences."
+              </p>
+            </Col>
+            <Col>
+              <p>Copyright ⓒ {year}</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
